74436eec22407b4d59511a90ab63d9b3fd44a187
Sabah <sabahtuna@gmail.com>
2016-03-19 18:05:18 +0200

    gizem

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+def post_list(request):  
+    posts = Post.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')   
+    return render(request, 'blog/post_list.html', { 'posts': posts})
